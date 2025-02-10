498c018abdea90a520370e92e2df9411bfe4be10
JiSeong Lee <cocoquiet@knu.ac.kr>
2025-02-10 10:34:55 +0900

    Feat: Impl blockchain

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/cocoquiet/cococoin/blockchain"
+)
+
+func main() {
+	chain := blockchain.GetBlockChain()
+
+	chain.AddBlock("Second Block")
+	chain.AddBlock("Third Block")
+	chain.AddBlock("Fourth Block")
+
+	for _, block := range chain.AllBlocks() {
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %s\n", block.Hash)
+		fmt.Printf("Prev Hash: %s\n", block.PrevHash)
+	}
 }
