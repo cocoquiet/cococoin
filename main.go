f140de8991e6cddb2b3ba0cf4d3674d6122f488b
JiSeong Lee <cocoquiet@knu.ac.kr>
2025-02-13 09:00:20 +0900

    Feat: Add rest module

--- main.go
+++ main.go
@@ -1,70 +1,7 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-	"log"
-	"net/http"
-
-	"github.com/cocoquiet/cococoin/blockchain"
-	"github.com/cocoquiet/cococoin/utils"
-)
-
-const port string = ":4000"
-
-type URL string
-
-func (u URL) MarshalText() ([]byte, error) {
-	url := fmt.Sprintf("http://localhost%s%s", port, u)
-	return []byte(url), nil
-}
-
-type URLDescription struct {
-	URL         URL    `json:"url"`
-	Method      string `json:"method"`
-	Description string `json:"descripsion"`
-	Payload     string `json:"payload,omitempty"`
-}
-
-type AddBlockBody struct {
-	Message string
-}
-
-func documentation(rw http.ResponseWriter, r *http.Request) {
-	data := []URLDescription{
-		{
-			URL:         URL("/"),
-			Method:      "GET",
-			Description: "See Documentation",
-		},
-		{
-			URL:         URL("/blocks"),
-			Method:      "POST",
-			Description: "Add A Block",
-			Payload:     "data:string",
-		},
-	}
-
-	rw.Header().Add("Content-Type", "application/json")
-	json.NewEncoder(rw).Encode(data)
-}
-
-func blocks(rw http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "GET":
-		rw.Header().Add("Content-Type", "application/json")
-		json.NewEncoder(rw).Encode(blockchain.GetBlockChain().AllBlocks())
-	case "POST":
-		var addBlockBody AddBlockBody
-		utils.HandleErr(json.NewDecoder(r.Body).Decode(&addBlockBody))
-		blockchain.GetBlockChain().AddBlock(addBlockBody.Message)
-		rw.WriteHeader(http.StatusCreated)
-	}
-}
+import "github.com/cocoquiet/cococoin/rest"
 
 func main() {
-	http.HandleFunc("/", documentation)
-	http.HandleFunc("/blocks", blocks)
-	fmt.Printf("Listening on http://localhost%s\n", port)
-	log.Fatal(http.ListenAndServe(port, nil))
+	rest.Start()
 }
